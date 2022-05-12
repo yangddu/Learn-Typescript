af8110131c1e0745159439ddd256d678d0bfa4b8
jooyoung <jooyoung@pood.pet>
2022-05-12 09:33:19 +0900

    add: string, number, array

---
+++ class-note/1.type-basic.ts
@@ -0,0 +1,13 @@
+//JS 문자열 선언
+// let str = 'hello';
+
+//TS 문자열 선언
+let str: string = 'hello';
+
+//TS 숫자
+let num: number = 12;
+
+//TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
