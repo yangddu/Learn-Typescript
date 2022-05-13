e0766fda0ffca5e4868b4ae849d79d6938ceea49
jooyoung <jooyoung@pood.pet>
2022-05-13 09:10:20 +0900

    add: parameter, return

---
+++ class-note/2.function.ts
@@ -0,0 +1,15 @@
+//함수의 파라미터에 타입을 정의하는 방식
+function fSum(a: number, b: number) {
+    return a + b;
+}
+fSum(10, 20);
+
+//함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    // return 10;
+}
+
+//함수에 타입을 정의하는 방식
+function sum02(a: number, b: number): number {
+    return a + b;
+}
