7fd08eec3fdb097e06b7bbe809b975f87da7fe06
jooyoung <jooyoung@pood.pet>
2022-05-16 22:39:52 +0900

    add: union type pros

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -6,8 +6,16 @@
 // logMessage(100);
 // logMessage(false);
 
+
+var jooooo: string | number | boolean;
 function logMessage(value: string | number) {
-    console.log(value);
+    if (typeof value === 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value === 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number');
 }
 
 logMessage('hello');
