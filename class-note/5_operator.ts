6a92c33a9b3d3a7995c859d7351eeeb40b39f3b1
jooyoung <jooyoung@pood.pet>
2022-05-18 09:38:21 +0900

    add: union type features

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -7,16 +7,30 @@
 // logMessage(false);
 
 
-var jooooo: string | number | boolean;
-function logMessage(value: string | number) {
-    if (typeof value === 'number') {
-        value.toLocaleString();
-    }
-    if (typeof value === 'string') {
-        value.toString();
-    }
-    throw new TypeError('value must be string or number');
+// var jooooo: string | number | boolean;
+// function logMessage(value: string | number) {
+//     if (typeof value === 'number') {
+//         value.toLocaleString();
+//     }
+//     if (typeof value === 'string') {
+//         value.toString();
+//     }
+//     throw new TypeError('value must be string or number');
+// }
+
+// logMessage('hello');
+// logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
 }
 
-logMessage('hello');
-logMessage(100);
+interface Person {
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+    someone.name
+}
