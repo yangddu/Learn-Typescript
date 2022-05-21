6b7c65e811c82b1aab23a4b33cde42f246a8ff92
jooyoung <jooyoung@pood.pet>
2022-05-21 09:36:56 +0900

    add: about generic

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -18,18 +18,27 @@
 //     return text;
 // }
 
-function logText(text: string | number) {
-    console.log(text);
-    return text;
-}
+// function logText(text: string | number) {
+//     console.log(text);
+//     return text;
+// }
 
-const a = logText('a');
-logText(10);
+// const a = logText('a');
+// logText(10);
 // function logNumber(num: number) {
 //     console.log(num)
 //     return num;
 // }
 
-logText('a');
-logText(10);
+function logText<T>(text: T): T {
+    console.log(text);
+    return text;
+}
+
+const str2 = logText<string>('abc')
+str.split('')
+const login = logText<boolean>(true);
+
+// logText('a');
+// logText(10);
 
