fdd1a029f2eb4c69111a5863c8d771456586e8e6
jooyoung <jooyoung@pood.pet>
2022-05-21 09:15:38 +0900

    add: function overlap type

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -2,12 +2,27 @@
 //   console.log(text);
 //   return text;
 // }
-// logText(10);
-// logText('하이');
-// logText(true);
+// logText(10); // 숫자 10
+// logText('하이'); // 문자열 하이 
+// logText(true); // 진위값 true
 
-function logText<T>(text: T):T {
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+// logText<string>('하이');
+
+function logText(text: string) {
     console.log(text);
+    // text.split('').reverse().join('');
     return text;
 }
-logText('하이');
+
+function logNumber(num: number) {
+    console.log(num)
+    return num;
+}
+
+logText('a');
+logNumber(10);
+
