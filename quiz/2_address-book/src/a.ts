2d22d40c44ad2af3a2f73c9cf00caae3cb631e64
jooyoung <jooyoung@pood.pet>
2022-05-23 10:09:40 +0900

    add: Promise API type

---
+++ quiz/2_address-book/src/a.ts
@@ -0,0 +1,14 @@
+// function fetchItems(): string[] {
+//     var items = ['a', 'b', 'c'];
+//     return items;
+// }
+// var result = fetchItems();
+// console.log(result);
+
+function fetchItems(): Promise<string[]> {
+    var items: string[] = ['a', 'b', 'c'];
+    return new Promise(function (resolve) {
+        resolve(items);
+    });
+}
+fetchItems();
