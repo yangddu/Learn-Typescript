f3d9e941185174eb5889d56371bbe03edd56ea3a
jooyoung <jooyoung@pood.pet>
2022-05-23 09:06:04 +0900

    restrict type

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -56,12 +56,23 @@ interface Dropdown<T> {
 const obj2: Dropdown<string> = { value: 'abc', selected: false };
 
 // 제네릭의 타입 제한
-function logTextLength<T>(text: T[]): T[] {
-    console.log(text.length);
-    text.forEach(function(text) {
-        console.log(text);
-    })
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach(function(text) {
+//         console.log(text);
+//     })
+//     return text;
+// }
+
+// logTextLength(['hi']);
+
+// 제네릭의 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+    text.length;
     return text;
 }
-
-logTextLength(['hi']);
+logTextLength('a');
+logTextLength({ length: 10 });
