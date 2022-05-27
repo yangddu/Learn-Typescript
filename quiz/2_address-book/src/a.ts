014e47db878fc977957360fa10c8b088aef1a408
jooyoung <jooyoung@pood.pet>
2022-05-27 19:39:38 +0900

    add: modularization

--- quiz/2_address-book/src/a.ts
+++ quiz/2_address-book/src/a.ts
@@ -5,6 +5,24 @@
 // var result = fetchItems();
 // console.log(result);
 
+export interface PhoneNumberDictionary {
+    [phone: string]: {
+      num: number;
+    };
+  }
+  
+export interface Contact {
+    name: string;
+    address: string;
+    phones: PhoneNumberDictionary;
+  }
+  
+export enum PhoneType {
+    Home = 'home',
+    Office = 'office',
+    Studio = 'studio'
+  }
+
 function fetchItems(): Promise<string[]> {
     var items: string[] = ['a', 'b', 'c'];
     return new Promise(function (resolve) {
